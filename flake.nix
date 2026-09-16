85bac01b0abbce48528e11fec519cba09ccc7992
Iskander Nafikov <nafikov.iskander@mail.ru>
2026-09-17 01:02:36 +0300

    feat: integrate Home Manager with nix-darwin

--- flake.nix
+++ flake.nix
@@ -8,24 +8,80 @@
       url = "github:nix-darwin/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+     home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    hm-dotfiles = {
+      url = "github:iskanred/nix-hm-dotfiles";
+      flake = false;
+    };
   };
 
-  outputs =
-    { self, nixpkgs, nix-darwin, ... }:
-    let
-      username = "iskanred";
-      hostname = "macbook";
-    in
-    {
-      darwinConfigurations.${hostname} =
-        nix-darwin.lib.darwinSystem {
-          specialArgs = {
-            inherit username;
-          };
+outputs =
+  {
+    self,
+    nixpkgs,
+    nix-darwin,
+    home-manager,
+    hm-dotfiles,
+    ...
+  }:
+  let
+    hosts = {
+      personal = {
+        hostname = "macbook";
+        username = "iskanred";
+        system = "aarch64-darwin";
+      };
 
-          modules = [
-            ./configuration.nix
-          ];
-        };
+      work = {
+        hostname = "work-macbook";
+        username = "YOUR_WORK_USERNAME";
+        system = "aarch64-darwin";
+      };
     };
+
+    mkDarwin =
+      {
+        hostname,
+        username,
+        system,
+      }:
+      nix-darwin.lib.darwinSystem {
+        specialArgs = {
+          inherit hostname username system;
+        };
+
+        modules = [
+          ./configuration.nix
+
+          home-manager.darwinModules.home-manager
+
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.extraSpecialArgs = {
+              local = {
+                inherit username system;
+                homeDirectory = "/Users/${username}";
+              };
+            };
+
+            home-manager.users.${username}.imports = [
+              "${hm-dotfiles}/home.nix"
+            ];
+          }
+        ];
+      };
+  in
+  {
+    darwinConfigurations = {
+      personal = mkDarwin hosts.personal;
+      work = mkDarwin hosts.work;
+    };
+  };
 }
