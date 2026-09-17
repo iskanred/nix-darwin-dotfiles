4ef4ffa2ffb37b9def9165a78420148f0c5e3a87
Iskander Nafikov <nafikov.iskander@mail.ru>
2026-09-18 01:02:26 +0300

    feat(keyboard): set Caps Lock for lang switch and Fn for emojis

--- configuration.nix
+++ configuration.nix
@@ -10,7 +10,21 @@
     home = local.homeDirectory;
   };
 
-  system.defaults.dock.show-recents = false;
+  system.defaults = {
+    dock.show-recents = false;
+
+    CustomUserPreferences = {
+      # Use Caps Lock to switch between Latin and non-Latin input sources.
+      NSGlobalDomain = {
+        TISRomanSwitchState = 1;
+      };
+
+      "com.apple.HIToolbox" = {
+        # Globe/Fn key -> Show Emoji & Symbols.
+        AppleFnUsageType = 2;
+      };
+    };
+  };
 
   nix.settings.experimental-features = [
     "nix-command"
